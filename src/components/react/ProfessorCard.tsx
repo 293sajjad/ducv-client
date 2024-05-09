d073034c0ae6bcb8905d3d3f0d1f17ff8ed9e179
293sajjad <293sajjad@gmail.com>
2024-05-09 18:09:35 +0330

    init

---
+++ src/components/react/ProfessorCard.tsx
@@ -0,0 +1,59 @@
+import { faStar, faStarHalf } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import type { FC } from "react";
+const renderStars = (rating: number) => {
+  const fullStars = Math.floor(rating);
+  const hasHalfStar = rating - fullStars >= 0.5;
+
+  const stars = Array.from({ length: fullStars }, (_, index) => (
+    <FontAwesomeIcon key={index} icon={faStar} />
+  ));
+
+  if (hasHalfStar) {
+    stars.push(<FontAwesomeIcon key="half-star" icon={faStarHalf} />);
+  }
+
+  return stars;
+};
+
+const ProfessorCard: FC<{
+  name: string;
+  family: string;
+  adjectives: string[] | string;
+  star: number;
+  img: string;
+}> = ({ name, family, adjectives, star, img }) => {
+  return (
+    <>
+      <div className="w-full md:w-1/2 lg:w-1/4 p-4">
+        <a href={`#`} className="text-black">
+          <div className="bg-white rounded-lg shadow-md p-4 h-full w-full lg:w-52">
+            <div className="mb-4">
+              <img
+                src={`http://localhost:1337${img}`}
+                alt={`${name} ${family}`}
+                className="w-full h-40 object-cover rounded"
+              />
+            </div>
+            <h2 className="text-xl font-semibold text-gray-800">
+              {`${name} ${family}`}
+            </h2>
+            <p className="text-gray-600 text-sm">
+              {adjectives.length == 1 ? adjectives : adjectives[0]}
+            </p>
+            {star == 0 ? (
+              <p className="mt-2 text-xs">هنوز امتیازی ثبت نشده</p>
+            ) : (
+              <div className="mt-2" dir="ltr">
+                {renderStars(star)}
+              </div>
+            )}
+            <div className="mt-2" dir="ltr"></div>
+          </div>
+        </a>
+      </div>
+    </>
+  );
+};
+
+export default ProfessorCard;
---
+++ src/components/react/ProfessorsLanding.tsx
@@ -0,0 +1,129 @@
+import axios from "axios";
+import { useEffect, useState, type FC } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar, faStarHalf } from "@fortawesome/free-solid-svg-icons";
+import ProfessorCard from "./ProfessorCard";
+
+interface Professor {
+  id: string;
+  attributes: {
+    name: string;
+    family: string;
+    adjectives: string;
+    scores: {
+      data: {
+        attributes: {
+          rate: number;
+        };
+      }[];
+    };
+    avatar: {
+      data: {
+        attributes: {
+          url: string;
+        };
+      };
+    };
+  };
+}
+
+const ProfessorLanding: FC<{ token: string; url: string }> = ({
+  token,
+  url,
+}) => {
+  const [professors, setProfessors] = useState<Professor[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<Error | null>(null);
+  const [searchQuery, setSearchQuery] = useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`${url}professors?populate=*`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        setProfessors(response.data.data);
+        setLoading(false);
+      } catch (error) {
+        setError(error);
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const filteredProfessors = professors.filter((professor) =>
+    `${professor.attributes.name} ${professor.attributes.family} ${professor.attributes.adjectives}`
+      .toLowerCase()
+      .includes(searchQuery.toLowerCase())
+  );
+
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(event.target.value);
+  };
+
+  const calculateStarRating = (professor: Professor) => {
+    const scores = professor.attributes.scores.data;
+    if (scores.length === 0) {
+      return 0;
+    }
+    const total = scores.reduce((acc, score) => acc + score.attributes.rate, 0);
+    return total / scores.length;
+  };
+
+  if (loading) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <div className="w-12 h-12 rounded-full animate-spin border-8 border-solid border-green-500 border-t-transparent"></div>
+      </div>
+    );
+  }
+
+  if (error) {
+    return <div>خطا در بارگیری اطلاعات: {error.message}</div>;
+  }
+
+  return (
+    <div>
+      {/* جستجو */}
+      <div className="mb-4" dir="rtl">
+        <input
+          type="text"
+          placeholder="جستجو..."
+          className="w-full px-4 py-2 rounded-lg border-gray-300 focus:outline-none focus:border-yellow-400"
+          value={searchQuery}
+          onChange={handleSearchChange}
+        />
+      </div>
+      {/* اصلی */}
+      {filteredProfessors.length == 0 ? (
+        <div
+          dir="rtl"
+          className="bg-orange-100 border-l-4 border-orange-500 text-orange-700 p-4"
+          role="alert"
+        >
+          <p className="font-bold">خطایی رخداد</p>
+          <p>استادی با این مشخصات در سیستم ثبت نشده است.</p>
+        </div>
+      ) : (
+        <div className="flex flex-wrap -mx-4">
+          {filteredProfessors.map((professor) => (
+            <ProfessorCard
+              key={professor.id}
+              name={professor.attributes.name}
+              family={professor.attributes.family}
+              adjectives={professor.attributes.adjectives}
+              star={calculateStarRating(professor)}
+              img={professor.attributes.avatar.data.attributes.url}
+            />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ProfessorLanding;
